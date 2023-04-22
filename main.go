fe5ca0f881899c7e1d38e594e6d60a050efe30d6
magnojunior07 <magnojunior07@proton.me>
2023-04-22 18:56:26 -0400

    testing goroutines

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
 
 func main() {
-	fmt.Println("Hello, World")
+	for i := 0; i < 20; i++ {
+		wg.Add(1) // increases WaitGroup
+		go work() // calls a function as goroutine
+	}
+
+	wg.Wait() // waits until WaitGroup is <= 0
+}
+
+func work() {
+	time.Sleep(time.Second)
+
+	var counter int
+
+	for i := 0; i < 1e10; i++ {
+		counter++
+	}
+
+	wg.Done()
 }
